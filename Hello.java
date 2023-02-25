2ca2c29640353ecbee6a4d9be2c3e0b507f95228
Jerry <jerrythottatil@gmail.com>
2023-02-25 22:20:14 +0530

    Added Hello.java file

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 public class Hello {
    
     public static void main(String[] args) {
-        int a=10;
-        System.out.println("Hello, World!"+a); 
+    
+        System.out.println("Hello, World!"); 
     }
     
 }
