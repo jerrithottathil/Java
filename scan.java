93d7feee8399307dd5b36417afdc53550ae40fc7
Jerry <jerrythottatil@gmail.com>
2023-02-28 15:15:05 +0530

    Added scan.java file

---
+++ scan.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class scan {
+  
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        
+        System.out.println("enter two numbers");
+        int a=s.nextInt();
+        int b=s.nextInt();
+        System.out.println(a+" "+b);
+    }
+    
+}
