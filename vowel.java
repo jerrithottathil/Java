b0272e5866eb799b1778ff821d7f983467612a50
Jerry <jerrythottatil@gmail.com>
2023-03-03 22:54:14 +0530

    Added vowel.java

---
+++ vowel.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+       
+      char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+   
+
+      for (int i = 0; i < vowels.length; ++ i) {
+        System.out.println(vowels[i]);
+      }
+    }
+   }
