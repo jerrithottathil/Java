e3982fd14d84b730b4d50838a919421dc0874324
Jerry <jerrythottatil@gmail.com>
2023-03-07 22:59:17 +0530

    Added palindrome.java file

---
+++ palindrome.java
@@ -0,0 +1,21 @@
+public class palindrome {
+    public static void main(String[] args) 
+    {
+        int n, sumeven= 0, sumodd = 0;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            a[i] = s.nextInt();
+        }
+        for(int i = 0; )
+    {
+    bin[i++] = n%2;
+       n = n/2;
+    }
+    for(int j = i-1;j >= 0;j--)
+    {
+       System.out.print(bin[j]);
+    }
+    }
